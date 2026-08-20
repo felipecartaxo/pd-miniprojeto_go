fb566785b9e000f80249e745301a378a6f2e1057
felipecartaxo <felipecartaxo1@gmail.com>
2026-08-20 20:40:51 -0300

    feat: organiza o codigo e implementa funcionalidade 2
    a funcionalidade 2 em questao eh referente a listagem de numeros

--- main.go
+++ main.go
@@ -6,11 +6,26 @@ import (
 
 func main() {
 
-	// Exibe o menu principal
+	// slice de inteiros inicialmente vazio
+	// que iremos manipular
+	numbers := []int{1, 2, 3}
+
+	// opcao informada pelo usuario
+	var option int
+
+	// exibe o menu principal
 	showMenu()
+
+	// leitura da opcao do usuario
+	fmt.Scan(&option) // & atribui o valor lido a variavel
+
+	if option == 2 {
+		listNumbers(numbers)
+	}
 }
 
 func showMenu() {
+
 	fmt.Println("Digite um número de 0 a 6: ")
 	fmt.Println("[1] Adicionar um número")
 	fmt.Println("[2] Listar os números")
@@ -19,4 +34,8 @@ func showMenu() {
 	fmt.Println("[5] Realizar divisão segura")
 	fmt.Println("[6] Resetar a lista")
 	fmt.Println("[0] Sair da aplicação")
+}
+
+func listNumbers(list []int) {
+	fmt.Println("Números: ", list)
 }
